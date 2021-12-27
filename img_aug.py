a20c8d2ccaed14e6ef0f76c3598eca9b40979c01
SpeagleYao <1065483675@qq.com>
2021-12-27 15:45:06 +0800

    class image augmentation

--- img_aug.py
+++ img_aug.py
@@ -5,33 +5,35 @@ import os
 import shutil
 import random
 
-# seed_value = 1105   # 设定随机数种子
-# np.random.seed(seed_value)
-# random.seed(seed_value)
-# os.environ['PYTHONHASHSEED'] = str(seed_value)  # 为了禁止hash随机化，使得实验可复现。
 
-data_in = np.load('./data/Data_in.npy')
-data_out = np.load('./data/Data_out.npy')
 
-p = Augmentor.DataPipeline(np.stack((data_in, data_out), axis=1))
+class data_generator():
+    def __init__(self, batch_size = 256, seed=None):
+        super(data_generator, self).__init__()
 
-p.rotate(probability=1.0, max_left_rotation=25, max_right_rotation=25)
-p.flip_left_right(probability=0.5)
-p.flip_top_bottom(probability=0.5)
-p.skew(probability=0.2)
-p.random_distortion(probability=0.2, grid_height=16, grid_width=16, magnitude=1)
-p.crop_random(probability=0.5, percentage_area=0.8)
-p.resize(probability=1, width=224, height=224)
+        data_in = np.load('./data/Data_in.npy')
+        data_out = np.load('./data/Data_out.npy')
 
-g = p.generator(batch_size=256)
+        if seed:
+            np.random.seed(seed)
+            random.seed(seed)
+            os.environ['PYTHONHASHSEED'] = str(seed)  # 为了禁止hash随机化，使得实验可复现。
 
-img_tot = np.array(next(g))
-img_aug = img_tot[:,0,:,:]
-gt_aug = img_tot[:,1,:,:]
-# Remember to do Normalization
-# print(np.mean(img_aug))
-# ind = 16
-# a = np.hstack((img_aug[ind], gt_aug[ind]))
-# cv2.imshow('img01', a)
-# cv2.waitKey(0)
-# print(np.array(img_aug).shape)
+        self.p = Augmentor.DataPipeline(np.stack((data_in, data_out), axis=1))
+
+        self.p.rotate(probability=1.0, max_left_rotation=25, max_right_rotation=25)
+        self.p.flip_left_right(probability=0.5)
+        self.p.flip_top_bottom(probability=0.5)
+        self.p.skew(probability=0.2)
+        self.p.random_distortion(probability=0.2, grid_height=16, grid_width=16, magnitude=1)
+        self.p.crop_random(probability=0.5, percentage_area=0.8)
+        self.p.resize(probability=1, width=224, height=224)
+
+        self.g = self.p.generator(batch_size)
+
+    def gen(self):
+        img_tot = np.array(next(self.g))
+        img_aug = img_tot[:,0,:,:]
+        gt_aug = img_tot[:,1,:,:]
+
+        return img_aug, gt_aug
